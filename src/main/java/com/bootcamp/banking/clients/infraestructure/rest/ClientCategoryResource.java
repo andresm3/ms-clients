b13daa717d9fd8fa49527d4678c99d309314d934
Andres Molero <andresmolero21@gmail.com>
2023-07-17 01:20:52 -0500

    fixes

--- src/main/java/com/bootcamp/banking/clients/infraestructure/rest/ClientCategoryResource.java
+++ src/main/java/com/bootcamp/banking/clients/infraestructure/rest/ClientCategoryResource.java
@@ -1,10 +1,10 @@
 package com.bootcamp.banking.clients.infraestructure.rest;
 
-import com.bootcamp.banking.clients.infraestructure.repository.ClientCategoryRepository;
-import com.bootcamp.banking.clients.domain.repository.dao.ClientCategoryDao;
-import com.bootcamp.banking.clients.infraestructure.rest.dto.ClientCategory;
-import com.bootcamp.banking.clients.infraestructure.rest.dto.ClientCategoryRequest;
+import com.bootcamp.banking.clients.application.ClientCategoryUseCases;
+import com.bootcamp.banking.clients.domain.dto.ClientCategoryRequest;
+import com.bootcamp.banking.clients.domain.models.ClientCategory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,33 +18,17 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/clientCategory")
 public class ClientCategoryResource {
 
-  private final ClientCategoryRepository clientCategoryRepository;
+  @Autowired
+  private ClientCategoryUseCases clientCategoryUseCases;
 
   @GetMapping
   public Flux<ClientCategory> getAll() {
-    return clientCategoryRepository.findAll()
-      .map(this::fromClientCategoryDaoToClientCategoryDto);
+    return clientCategoryUseCases.getAll();
   }
 
   @PostMapping
   public Mono<ClientCategory> createClientCategory(
     @RequestBody ClientCategoryRequest clientCategoryRequest) {
-    return clientCategoryRepository.save(
-        this.fromClientCategoryRequestToClientCategoryDao(clientCategoryRequest))
-      .map(this::fromClientCategoryDaoToClientCategoryDto);
-  }
-
-  private ClientCategory fromClientCategoryDaoToClientCategoryDto(ClientCategoryDao dao) {
-    ClientCategory clientCategory = new ClientCategory();
-    clientCategory.setId(dao.getId());
-    clientCategory.setClientCategory(dao.getCategoryName());
-    return clientCategory;
-  }
-
-  private ClientCategoryDao fromClientCategoryRequestToClientCategoryDao(
-    ClientCategoryRequest clientCategoryRequest) {
-    ClientCategoryDao clientCategoryDao = new ClientCategoryDao();
-    clientCategoryDao.setCategoryName(clientCategoryRequest.getCategoryName());
-    return clientCategoryDao;
+    return clientCategoryUseCases.createClientCategory(clientCategoryRequest);
   }
 }
