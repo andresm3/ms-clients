f936891b821e69891d1571cf50aa431fd80f22cd
Andres Molero <andresmolero21@gmail.com>
2023-07-30 15:51:17 -0500

    fix config server

--- src/main/java/com/bootcamp/banking/clients/ClientsApplication.java
+++ src/main/java/com/bootcamp/banking/clients/ClientsApplication.java
@@ -3,6 +3,7 @@ package com.bootcamp.banking.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableEurekaClient
 @SpringBootApplication
 public class ClientsApplication {
 
